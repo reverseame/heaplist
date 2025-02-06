b8204053d8216055fce301a21289703f62eb5e34
Daniel Uroz <duroz@unizar.es>
2025-02-06 11:54:34 +0100

    Plugin ported to use Volatility 3 utilities to traverse structures

--- README.md
+++ README.md
@@ -1,52 +1,62 @@
-# Heap Extraction Plugin for Volatility3
+# Heap Extraction Plugin for Volatility 3
 
-## Overview
+This plugin for [Volatility 3](https://github.com/volatilityfoundation/volatility3/) allows forensic analysts to extract heap entries from processes of a Windows memory image. The plugin is particularly useful for identifying and extracting information that may be critical to a forensic investigation, such as injected code, sensitive data, and other relevant process behavior.
 
-This plugin is designed for use with the Volatility3 framework. It allows forensic analysts to extract heap fragments from process memory dumps in Windows systems. The plugin is particularly useful for identifying and extracting information that may be critical in forensic investigations, such as injected code, sensitive data, and other relevant process behaviors.
-
+**NOTE**: This plugin only supports the traditional NT Heap, the modern [Segment Heap](https://learn.microsoft.com/en-us/windows/win32/sbscs/application-manifests#heaptype) implementation introduced in Windows 10 is not currently supported.
 
 ## Features
 
-- Lists all heaps of a specific process in a Windows memory dump.
-- Extracts specific heap fragments based on the analyst's interest.
-- Handles encoded heap fragments by decoding them for analysis.
-- Outputs extracted heap data into files for further examination.
-
+- List all heap entries of a given process in a Windows memory image.
+- Dump the decoded heap data to a file for further investigation.
 
 ## Installation
 
-To install the plugin, follow these steps:
+1. Install [Volatility 3](https://github.com/volatilityfoundation/volatility3?tab=readme-ov-file#installing). **NOTE**: This plugin requires at least version 2.0.0.
+1. Clone this repository.
+3. Copy the plugin file `heaplist.py` into the default plugin directory of Volatility 3:
 
-1. Clone or download this repository to your local machine.
-2. Ensure that you have Volatility3 installed and properly configured. You can follow the official installation guide [here](https://github.com/volatilityfoundation/volatility3). NOTE: For this plugin to function correctly, the minimum required version of the Volatility3 framework is 2.0.0.
-3. Copy the plugin file into the appropriate Volatility3 plugin directory:
-
-    ```bash
-    cp heaplist.py /path/to/volatility3/volatility3/plugins/windows/
-    ```
-
-4. The plugin is now ready to use with Volatility3.
+```shell
+$ cp heaplist/heaplist.py /path/to/volatility3/volatility3/plugins/windows
+```
 
 ## Usage
 
-To use the plugin, run Volatility3 with the following command:
+```
+usage: volatility windows.heaplist [-h] [--pid [PID [PID ...]]] [--dump DUMP] [--dump-all]
 
-```bash
-vol.py -f <memory_dump> windows.heaplist.HeapList [options]
- ```
+Lists the NT heap entries of processes from a Windows memory image.
 
-## Options
-
-- --pid: Specify the process ID (PID) to analyze. If not specified, the plugin will analyze all processes.
-- --dump-all: Dump all heap fragments from the specified process or processes.
-- --dump: Specify the address of the heap fragment to download.
-
-
-## Example
-To analyze the heaps of a specific process with PID 1234 and dump all heap fragments:
-
-```bash
-vol.py -f memory.dmp windows.heaplist.HeapList -p 1234 --dump-all
+optional arguments:
+  -h, --help            show this help message and exit
+  --pid [PID [PID ...]]
+                        Process ID to include (all other processes are excluded)
+  --dump DUMP           Virtual memory address of the heap entry to dump
+  --dump-all            Extract all heap entries
 ```
 
+## Example
 
+List and dump heap entries of a specific process with PID 7540:
+
+```
+$ vol3.py -f /path/to/windows-memory-image.raw windows.heaplist --pid 7540 --dump-all
+Volatility 3 Framework 2.11.0
+Progress:  100.00               PDB scanning finished                        
+PID     Name    Heap    Segment Entry   Size    Flags   State   Data    File Output
+
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090000   0x740   [01]    busy    ........................................        7540.heap.1edba090000.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090740   0x30    [01]    busy    p........................}......        7540.heap.1edba090740.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090770   0x60    [01]    busy    @.......@...............................        7540.heap.1edba090770.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba0907d0   0x50    [01]    busy    C.:.\.W.i.n.d.o.w.s.\.S.y.s.t.e.m.3.2.\.        7540.heap.1edba0907d0.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090820   0x30    [01]    busy    0...............................        7540.heap.1edba090820.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090850   0x110   [01]    busy    ................@...............\..5*..L        7540.heap.1edba090850.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090960   0x1e0   [01]    busy    ........`...............................        7540.heap.1edba090960.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090b40   0x1e0   [01]    busy    ................0.......H.......`.......        7540.heap.1edba090b40.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090d20   0x50    [01]    busy    0.......0...............H.......H.......        7540.heap.1edba090d20.dmp
+7540    OneDrive.exe    0x1edba090000   0x1edba090000   0x1edba090d70   0x20    [01]    busy    ................        7540.heap.1edba090d70.dmp
+[...redacted...]
+```
+
+## License
+
+Licensed under the [GNU GPLv3 license](./LICENSE).
--- heaplist.py
+++ heaplist.py
@@ -1,40 +1,45 @@
+import enum
 import logging
-from typing import Callable, List, Generator, Iterable, Type, Optional
+import string
+from typing import Generator, List
 
-# This plugin for Volatility3 analyzes heaps of processes in a Windows system.
-# It allows enumerating heaps and, optionally, extracting memory chunks from these heaps.
-# The chunks can be filtered by address or extracted entirely.
-
-from volatility3.framework import exceptions, interfaces, renderers, objects
+from volatility3.framework import exceptions, interfaces, renderers, constants
 from volatility3.framework.configuration import requirements
 from volatility3.plugins.windows import pslist
+from volatility3.framework.objects import utility
 from volatility3.framework.renderers import format_hints
-from enum import IntFlag
 
-# Setting up the logger
 vollog = logging.getLogger(__name__)
 
-# Flags for heap entries (_HEAP_ENTRY)
-class ENTRY_FLAGS(IntFlag):
-    BUSY = 0x01
-    EXTRA = 0x02
-    FILL = 0x04
-    #VIRTUAL = 0x08
-    LAST = 0x10
-    FLAG1 = 0x20
-    FLAG2 = 0x40
-    FLAG3 = 0x80
-    FLAGS = FLAG1 | FLAG2 | FLAG3
+# Flags for heap entries (HEAP_ENTRY)
+class HEAP_ENTRY_FLAGS(enum.IntFlag):
+    HEAP_ENTRY_BUSY             = 0x01
+    HEAP_ENTRY_EXTRA_PRESENT    = 0x02
+    HEAP_ENTRY_FILL_PATTERN     = 0x04
+    HEAP_ENTRY_VIRTUAL_ALLOC    = 0x08
+    HEAP_ENTRY_LAST_ENTRY       = 0x10
+    HEAP_ENTRY_SETTABLE_FLAG1   = 0x20
+    HEAP_ENTRY_SETTABLE_FLAG2   = 0x40
+    HEAP_ENTRY_SETTABLE_FLAG3   = 0x80
 
-# Main plugin class that implements the functionality to analyze and extract
-# information from heaps in selected processes.
+# Custom HEAP_ENTRY display names
+HEAP_ENTRY_FLAGS_DISPLAY_NAMES = {
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_BUSY:           "busy",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_EXTRA_PRESENT:  "extra",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_FILL_PATTERN:   "fill",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_VIRTUAL_ALLOC:  "internal",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_LAST_ENTRY:     "last",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_SETTABLE_FLAG1: "user_flag1",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_SETTABLE_FLAG2: "user_flag2",
+    HEAP_ENTRY_FLAGS.HEAP_ENTRY_SETTABLE_FLAG3: "user_flag3",
+}
+
 class HeapList(interfaces.plugins.PluginInterface):
-    """Prints the PIDs, NT Heap Address for Backend Layer, Segment Address and Chunk address, size and status"""
+    """Lists the NT heap entries of processes from a Windows memory image."""
 
     _required_framework_version = (2, 0, 0)
+    _version = (1, 0, 0)
 
-    # Defines the requirements needed to run the plugin, including the kernel,
-    # the pslist plugin, and optional parameters like PID and dump options.
     @classmethod
     def get_requirements(cls) -> List[interfaces.configuration.RequirementInterface]:
         return [
@@ -46,212 +51,136 @@ class HeapList(interfaces.plugins.PluginInterface):
             requirements.PluginRequirement(
                 name="pslist", plugin=pslist.PsList, version=(2, 0, 0)
             ),
-            requirements.IntRequirement(
+            requirements.ListRequirement(
                 name="pid",
+                element_type=int,
                 description="Process ID to include (all other processes are excluded)",
                 optional=True,
             ),
-            requirements.StringRequirement(
+            requirements.IntRequirement(
                 name="dump",
-                description="Virtual memory address of the process to start the dump.",
+                description="Virtual memory address of the heap entry to dump",
                 optional=True,
-                default=None,
             ),
             requirements.BooleanRequirement(
                 name="dump-all",
-                description="Extract all memory fragments from the heap",
+                description="Extract all heap entries",
                 default=False,
                 optional=True,
             ),
         ]
 
-    # Generator that iterates over the selected processes and produces heap information.
-    # If specified, it also extracts memory chunks and saves them to files.
-    def _generator(self, procs):
+    def read_heap_entry_data(self, heap_entry: int, heap_entry_size: int, granularity: int) -> bytes:
+        trans_layer = self.context.layers[heap_entry.vol.layer_name]
+        return trans_layer.read(heap_entry.vol.offset + granularity, heap_entry_size - granularity)
+
+    def flag_to_string(self, flag: int) -> str:
+        flag_encoded = HEAP_ENTRY_FLAGS(flag)
+
+        if HEAP_ENTRY_FLAGS.HEAP_ENTRY_BUSY in flag_encoded:
+            string = HEAP_ENTRY_FLAGS_DISPLAY_NAMES.get(HEAP_ENTRY_FLAGS.HEAP_ENTRY_BUSY)
+        else:
+            string = "free"
+
+        for f in list(HEAP_ENTRY_FLAGS)[1:]:
+            if f in flag_encoded:
+                string += f" {HEAP_ENTRY_FLAGS_DISPLAY_NAMES.get(f, f.name)}"
+
+        return string
+
+    def _generator(self, procs: Generator[interfaces.objects.ObjectInterface, None, None]):
         kernel = self.context.modules[self.config["kernel"]]
 
-        # Check if a specific dump address was provided
-        dump_address = self.config.get('dump')
-        dump_mode = dump_address is not None
+        _HEAP = kernel.get_type("_HEAP")
+        _HEAP_ENTRY = kernel.get_type("_HEAP_ENTRY")
+        granularity = _HEAP_ENTRY.size
 
         for proc in procs:
-            pid = "Unknown"
-            process_name = "Unknown"
-            number_of_heaps = "Unknown"
+            pid = proc.UniqueProcessId
+            proc_name = proc.ImageFileName.cast("string", max_length=proc.ImageFileName.vol.count, errors="replace")
 
             try:
-                # Attempt to get the PEB of the process and then the associated heaps.
-                pid = proc.UniqueProcessId
-                process_name = proc.ImageFileName.cast("string", max_length=proc.ImageFileName.vol.count, errors='replace')
-                peb = proc.get_peb()
-                number_of_heaps = peb.NumberOfHeaps
-                HEAP_ENTRY = kernel.get_type("_HEAP_ENTRY")
-                HEAP_SEGMENT = kernel.get_type("_HEAP_SEGMENT")
-                HEAP = kernel.get_type("_HEAP")
-                LIST_ENTRY = kernel.get_type("_LIST_ENTRY")
-                granularity = HEAP_ENTRY.size
-                process_heaps = peb.ProcessHeaps.dereference()
-                symbol_table = proc._context.symbol_space[proc.get_symbol_table_name()]
-                heap_type = symbol_table.get_type('pointer')
-                heap_pointer_array = process_heaps.cast('array', count=number_of_heaps, subtype=heap_type)
+                try:
+                    peb = proc.get_peb()
+                except exceptions.InvalidAddressException:
+                    vollog.warning(f"{proc_name} ({pid})\t: Unable to read the _PEB")
+                    continue
 
-                # Iterate over each heap in the process to analyze its segments and entries.
-                for heap_index, heap_pointer in enumerate(heap_pointer_array):
-                    process_heap = heap_pointer.dereference()
-                    heap_final = process_heap.vol.offset
-                    offset = heap_final
-                    heap: objects.StructType = self.context.object(HEAP, peb.vol.layer_name, heap_final)
-                    FlinkStart = heap.SegmentList.Flink
-                    Flink = FlinkStart
-                    FlinkFinal = heap.SegmentList.Blink
-                    indexSegments = 1
-                    
-                    # Start traversing the segment list in the heap using the doubly linked list pattern.
-                    while FlinkStart != FlinkFinal or indexSegments == 1:
-                        offset = Flink - 24
-                        segment: objects.StructType = self.context.object(HEAP_SEGMENT, heap.vol.layer_name, offset)
-                        SegmentPrint = offset
-                        
-                        while True:
-                            try:
+                heap_pointers = utility.array_of_pointers(
+                        peb.ProcessHeaps.dereference(),
+                        count=peb.NumberOfHeaps,
+                        subtype=_HEAP,
+                        context=self.context,
+                    )
+
+                for heap in heap_pointers:
+                    for segment in heap.SegmentList.to_list(f"{kernel.symbol_table_name}{constants.BANG}_HEAP_SEGMENT", "SegmentListEntry"):
+                        heap_entry_addr = segment.BaseAddress
+
+                        # TODO: Check for uncommitted regions instead of relying on PagedInvalidAddressException
+                        try:
+                            while heap_entry_addr < segment.LastValidEntry:
+                                heap_entry = self.context.object(_HEAP_ENTRY, heap.vol.layer_name, heap_entry_addr)
+
+                                heap_entry_size = heap_entry.Size
+                                heap_entry_flags = heap_entry.Flags
+
+                                if heap.EncodeFlagMask == 0x100000:
+                                    heap_entry_size ^= heap.Encoding.Size
+                                    heap_entry_flags ^=  heap.Encoding.Flags
+
+                                heap_entry_size *= granularity
+
+                                data = self.read_heap_entry_data(heap_entry, heap_entry_size, granularity)
+
                                 file_output = "Disabled"
-                                # Get the current heap entry and decode it (if encoded).
-                                # Then attempt to read and decode the first 40 bytes of the chunk to extract printable data.
-                                entry: objects.StructType = self.context.object(HEAP_ENTRY, heap.vol.layer_name, offset)
-                                encoding = heap.EncodeFlagMask
-                                if encoding == 1048576:
-                                    size = (entry.Size ^ heap.Encoding.Size)
-                                    AllSize = size * granularity
-                                    Flags = (entry.Flags ^ heap.Encoding.Flags)
-                                    cipher = 'Enabled'
-                                else:
-                                    size = (entry.Size)
-                                    AllSize = size * granularity
-                                    Flags = entry.Flags
-                                    cipher = 'Disabled'
-                                
-                                # Skip this entry if the calculated size is 0x0
-                                if AllSize == 0x0:
-                                    offset += granularity  # or another appropriate value to move to the next entry
-                                    continue  # Skip the current entry and move to the next
 
-                                trans_layer = self.context.layers[entry.vol.layer_name]
-                                data = trans_layer.read(entry.vol.offset + granularity, (size - 1) * granularity)
-                                try:
-                                    user_data = data[:40].decode('utf-8', errors='ignore')
-                                    user_data = ''.join(c if c.isprintable() else '.' for c in user_data)
-                                except UnicodeDecodeError:
-                                    user_data = "Cannot decode"
+                                if self.config["dump-all"] or (self.config["dump"] == heap_entry_addr):
+                                    file_output = f'{pid}.heap.{heap_entry_addr:x}.dmp'
+                                    with open(file_output, 'wb') as f:
+                                        f.write(data)
 
-                                # If dump-all is enabled, attempt to dump the chunk data to a file.
-                                if self.config["dump-all"]:
-                                    dump = hex(entry.vol.offset)
-                                    file_output = f"{pid}.{process_name}.{dump}.dmp"
-                                    file_name = file_output
-                                    try:
-                                        with open(file_name, 'wb') as file_handle:
-                                            file_handle.write(data)
-                                    except Exception as e:
-                                        print(f"An error occurred when writing in the file: {e}")
+                                decoded_data = ''.join([c if (c in string.printable) and (c not in string.whitespace) else "." for c in data[:40].decode('ascii', errors="replace").replace("\ufffd", ".")])
 
-                                # If a specific dump address is provided, dump only that chunk.                                
-                                if dump_mode:
-                                    dump = hex(entry.vol.offset)
-                                    if dump == dump_address:
-                                        file_output = f"{pid}.{process_name}.{dump}.dmp"
-                                        file_name = file_output
-                                        try:
-                                            with open(file_name, 'wb') as file_handle:
-                                                file_handle.write(data)
-                                        except Exception as e:
-                                            print(f"An error occurred while writing in the file: {e}")
-                                        
-                                        # Only show output for the specified dump address.
-                                        yield (
-                                            0,
-                                            (
-                                                pid,
-                                                process_name,
-                                                format_hints.Hex(heap_final),
-                                                format_hints.Hex(SegmentPrint),
-                                                format_hints.Hex(entry.vol.offset),
-                                                cipher,
-                                                format_hints.Hex(AllSize),
-                                                flags_str,
-                                                user_data,
-                                                file_output,
-                                            )
-                                        )
-                                        return  # Exit after handling the specific chunk
-                                    
-                                # Create a list of active flags for the current heap entry and convert them to a string.
-                                flags_activos = []
-                                for flag in ENTRY_FLAGS:
-                                    if Flags & flag:
-                                        flags_activos.append(flag.name)
-                                flags_str = '[' + ' '.join(str(flag) for flag in flags_activos) + ']'
+                                yield (
+                                    0,
+                                    (
+                                        pid,
+                                        proc_name,
+                                        format_hints.Hex(heap.BaseAddress),
+                                        format_hints.Hex(segment.BaseAddress),
+                                        format_hints.Hex(heap_entry_addr),
+                                        format_hints.Hex(heap_entry_size),
+                                        f"[{heap_entry_flags:02x}]",
+                                        self.flag_to_string(heap_entry_flags),
+                                        decoded_data,
+                                        file_output
+                                    )
+                                )
 
-                                offset += AllSize
+                                heap_entry_addr += heap_entry_size
+                        except exceptions.PagedInvalidAddressException as e:
+                            vollog.debug(f"{proc_name} ({pid})\t: Unable to read _HEAP_ENTRY @ {heap_entry_addr:#x}\t: {str(e)}")
+            except exceptions.SwappedInvalidAddressException as e:
+                vollog.warning(f"{proc_name} ({pid})\t: Unable to read memory due to memory paging @ {e.invalid_address:#x}")
 
-                                # Yield information about the current heap entry if not in dump mode.
-                                if not dump_mode:
-                                    yield (
-                                        0,
-                                        (
-                                            pid,
-                                            process_name,
-                                            format_hints.Hex(heap_final),
-                                            format_hints.Hex(SegmentPrint),
-                                            format_hints.Hex(entry.vol.offset),
-                                            cipher,
-                                            format_hints.Hex(AllSize),
-                                            flags_str,
-                                            user_data,
-                                            file_output,
-                                        )
-                                    )           
-
-                            except exceptions.PagedInvalidAddressException:
-                                break
-                        
-                        if FlinkStart == FlinkFinal:
-                            break
-
-                        List: objects.StructType = self.context.object(LIST_ENTRY, heap.vol.layer_name, Flink)
-                        Flink = List.Flink
-                        FlinkFinal = List.Blink
-                        indexSegments += 1
-
-            except exceptions.InvalidAddressException as excp:
-                vollog.debug(f"Process {pid}: invalid address {excp.invalid_address} in layer {excp.layer_name}")
-                continue
-            except Exception as e:
-                vollog.debug(f"Error obtaining information for process {pid}: {e}")
-                continue
-    
-    # Main method executed when the plugin is run. It retrieves the process list
-    # and constructs the TreeGrid with the heap information.
     def run(self):
-        filter_func = pslist.PsList.create_pid_filter([self.config.get("pid", None)])
         kernel = self.context.modules[self.config["kernel"]]
-        processes = pslist.PsList.list_processes(
-            context=self.context,
-            layer_name=kernel.layer_name,
-            symbol_table=kernel.symbol_table_name,
-            filter_func=filter_func,
-        )
+        filter_func = pslist.PsList.create_pid_filter(self.config.get("pid", None))
+
         return renderers.TreeGrid(
             [
                 ("PID", int),
-                ("Process Name", str),
-                ("Heap Address", format_hints.Hex),
-                ("Segment Address", format_hints.Hex),
-                ("Chunk Address", format_hints.Hex),
-                ("Coded", str),
-                ("Chunk Size", format_hints.Hex),
-                ("Chunk Status", str),
-                ("Payload", str),
-                ("File Output", str),
+                ("Name", str),
+                ("Heap", format_hints.Hex),
+                ("Segment", format_hints.Hex),
+                ("Entry", format_hints.Hex),
+                ("Size", format_hints.Hex),
+                ("Flags", str),
+                ("State", str),
+                ("Data", str),
+                ("File Output", str)
+
             ],
             self._generator(
                 pslist.PsList.list_processes(
